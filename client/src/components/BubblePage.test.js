f130964f69c548cbee8889c36f8cc3923bbe4bfc
cladams0203 <cladams0203@gmail.com>
2020-11-19 13:43:31 -0500

    app functioning and working on testing

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+// import { renderHook } from "@testing-library/react-hooks";
 import BubblePage from "./BubblePage";
+// import { useFetch } from "../utils/apiHook";
+// import { api } from "../utils/api";
 
 test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+  const { getByText, debug } = render(<BubblePage />);
+  debug();
 });
