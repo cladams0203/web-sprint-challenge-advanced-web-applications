5fae0bb768e89b79cf992fa0473fbae8585f5ea9
cladams0203 <cladams0203@gmail.com>
2020-11-20 12:38:18 -0500

    finished bubble page test

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-// import { renderHook } from "@testing-library/react-hooks";
+import { render, screen, waitFor, wait } from "@testing-library/react";
 import BubblePage from "./BubblePage";
-// import { useFetch } from "../utils/apiHook";
-// import { api } from "../utils/api";
+import * as mockApi from "../utils/api";
+import axios from "axios";
 
-test("Fetches data and renders the bubbles", () => {
+jest.mock("../utils/api");
+jest.mock("axios", () => ({
+  create: jest.fn(),
+  get: jest.fn(),
+}));
+
+const testColors = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+];
+
+test("Fetches data and renders the bubbles", async () => {
+  const res = jest.spyOn(mockApi, "api");
+  res.mockImplementation(() => {
+    return axios;
+  });
+  axios.get.mockResolvedValueOnce({ data: testColors });
   const { getByText, debug } = render(<BubblePage />);
+  debug();
+  const resolved = await wait(() => {});
   debug();
 });
